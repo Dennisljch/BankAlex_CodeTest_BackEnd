a51529446568d716a67d03f31c1e0f763f0dc1d6
Dennis <dennisljchit@gmail.com>
2021-08-15 17:46:00 +1000

    Add customer and Transaction class

---
+++ SharedLibrary/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLibrary.Models
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
