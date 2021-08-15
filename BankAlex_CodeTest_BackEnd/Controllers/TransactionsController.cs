2a6f920bbdc91ee19be7315e23231f74e431e284
Dennis <dennisljchit@gmail.com>
2021-08-15 16:49:38 +1000

    Add project files.

---
+++ BankAlex_CodeTest_BackEnd/Controllers/TransactionsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankAlex_CodeTest_BackEnd.Controllers
+{
+    [Route("api/transactions")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        [HttpGet(Name = nameof(GetTransaction))]
+        public IActionResult GetTransaction()
+        {
+            var response = new
+            {
+                href = Url.Link(nameof(GetTransaction), null)
+            };
+
+            return Ok(response);
+        }
+    }
+}
